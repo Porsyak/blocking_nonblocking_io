88a8ab1b720e19cfc27928a2e61157f31740b7bd
Ivan Porsyak <2663634@gmail.com>
2022-07-22 16:20:19 +0300

    Fix method "deleteSpaces"

--- src/BlockingClientServer/ClientServer.java
+++ src/BlockingClientServer/ClientServer.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 
 public class ClientServer {
     public static String deleteSpaces(String string) {
-        return string.trim();
+        return string.replaceAll(" ", "");
     }
 
 }
